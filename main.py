8beb78fde55ce5a9404bf96c143c2674b7f0147b
LukeRoberson <luke.robertson@networkdirection.net>
2025-05-15 13:36:27 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+The security service
+
+Used for:
+    - Authenticating users with an iDP
+    - Encrypting and decrypting data
+
+Usage:
+    Run this module to start the security service.
+
+Example:
+    $ python main.py
+"""
+
+pass
