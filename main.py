455337f2707d68849a34743c7f7d8ecd607fc8e5
LukeRoberson <luke.robertson@networkdirection.net>
2025-06-11 13:52:45 +1000

    Updated config API URL

--- main.py
+++ main.py
@@ -20,6 +20,14 @@ Usage:
         a Docker container.
     Build the Docker image and run it with the provided Dockerfile.
 
+Functions:
+    - fetch_global_config:
+        Fetches the global configuration from the web interface.
+    - logging_setup:
+        Sets up the root logger for the web service.
+    - create_app:
+        Creates the Flask application instance and sets up the configuration.
+
 Blueprints:
     - security_api: Handles security-related API endpoints
 
@@ -40,64 +48,101 @@ import requests
 import logging
 
 from azure import azure_auth
-from systemlog import system_log
 from api import security_api
 
 
-# Get global config
-global_config = None
-try:
-    response = requests.get("http://web-interface:5100/api/config", timeout=3)
-    response.raise_for_status()  # Raise an error for bad responses
-    global_config = response.json()
+CONFIG_URL = "http://core:5100/api/config"
 
-except Exception as e:
-    logging.critical(
-        "Failed to fetch global config from web interface."
-        f" Error: {e}"
+
+def fetch_global_config(
+    url: str = CONFIG_URL,
+) -> dict:
+    """
+    Fetch the global configuration from the core service.
+
+    Args:
+        None
+
+    Returns:
+        dict: The global configuration loaded from the core service.
+
+    Raises:
+        RuntimeError: If the global configuration cannot be loaded.
+    """
+
+    global_config = None
+    try:
+        response = requests.get(url, timeout=3)
+        response.raise_for_status()
+        global_config = response.json()
+
+    except Exception as e:
+        logging.critical(
+            "Failed to fetch global config from core service."
+            f" Error: {e}"
+        )
+
+    if global_config is None:
+        raise RuntimeError("Could not load global config from core service")
+
+    return global_config['config']
+
+
+def logging_setup(
+    config: dict,
+) -> None:
+    """
+    Set up the root logger for the web service.
+
+    Args:
+        config (dict): The global configuration dictionary
+
+    Returns:
+        None
+    """
+
+    # Get the logging level from the configuration (eg, "INFO")
+    log_level_str = config['web']['logging-level'].upper()
+    log_level = getattr(logging, log_level_str, logging.INFO)
+
+    # Set up the logging configuration
+    logging.basicConfig(
+        level=log_level,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     )
-
-if global_config is None:
-    raise RuntimeError("Could not load global config from web interface")
-
-# Set up logging
-log_level_str = global_config['config']['web']['logging-level'].upper()
-log_level = getattr(logging, log_level_str, logging.INFO)
-logging.basicConfig(level=log_level)
-logging.info("Logging level set to: %s", log_level_str)
-
-# Create the Flask application
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('api_master_pw')
-app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SESSION_FILE_DIR'] = '/app/flask_session'
-app.config['GLOBAL_CONFIG'] = global_config['config']
-Session(app)
-
-# Register the security API blueprint
-app.register_blueprint(security_api)
+    logging.info("Logging setup complete with level: %s", log_level)
 
 
-# Log startup message
-system_log.log("The security service is starting")
+def create_app(
+    config: dict,
+) -> Flask:
+    """
+    Create the Flask application instance and set up the configuration.
+    Registers the necessary blueprints for the web service.
 
-# Register authentication blueprint
-app.register_blueprint(azure_auth)
+    Args:
+        config (dict): The global configuration dictionary
 
-# Log 'started' message
-system_log.log("The security service has started")
+    Returns:
+        Flask: The Flask application instance.
+    """
+
+    # Create the Flask application
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('api_master_pw')
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.config['SESSION_FILE_DIR'] = '/app/flask_session'
+    app.config['GLOBAL_CONFIG'] = config
+    Session(app)
+
+    # Register blueprints
+    app.register_blueprint(security_api)
+    app.register_blueprint(azure_auth)
+
+    return app
 
 
-'''
-NOTE: When running in a container, the host and port are set in the
-    uWSGI config. uWSGI starts the process, which means the
-    Flask app is not run directly.
-    This can be uncommented for local testing.
-'''
-# if __name__ == "__main__":
-#     # Run the application
-#     app.run(
-#         debug=True,
-#         host='0.0.0.0',
-#         port=5000,
-#     )
+# Setup the security service
+global_config = fetch_global_config(CONFIG_URL)
+logging_setup(global_config)
+app = create_app(global_config)
